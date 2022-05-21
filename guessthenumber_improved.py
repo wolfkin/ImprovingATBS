d8b4992586df55eff2952ee836e815769a587c83
wolfkin-gungrave <wolfkin@gmail.com>
2022-05-20 22:43:59 -0400

    change number works
    was having issues with being able to change the variables and everyone I went to for help sucks but I figured it out on my own which is fine for me but speaks poorly of the type of help i can expect in the future.
    
    Also learned about string formatting so %s ing all over the place.

--- guessthenumber_improved.py
+++ guessthenumber_improved.py
@@ -4,58 +4,85 @@ import random
 
 continueFlag = 1
 user = ""
+low = int(1)
+high = int(20)
 
 print('Welcome to the improved number guessing game\n\n')
 
 def nameCheck(word):
-	while word != "":
-		print('Hello ' + word + '. Would you like to change your name?')
-		changeName = input().lower()
-		if changeName[0] == 'n':
-			print('Alright keep playing ' + word)
-			return word
-		word = input('Hello. What is your name? -->')
-		return word
-	word = input('Hello. What is your name? -->')
-	return word
+    while word != "":
+        print('Hello ' + word + '. Would you like to change your name?')
+        changeName = input().lower()
+        if changeName[0] == 'n':
+            print('Alright keep playing %s' % (word))
+            return word
+        word = input('Hello. What is your name? -->')
+        return word
+    word = input('Hello. What is your name? -->')
+    return word
+
+def debug_settings():
+    global low, high
+    print('Entering Debug Mode')
+    print(' ___        _           __ _ ')
+    print('|   \  ___ | |__  _  _ / _` |')
+    print('| |) |/ -_)|  _ \| || |\__. |')
+    print('|___/ \___||____/ \_._||___/ ')
+    print('Debug Mode: On')
+    print('\nDebug Mode: Exit is ' + str(exitValues) + '<<<\n\n')
+    high  =change_number(high,"High")
+    low   =change_number(low,"Low")
+    
+def change_number(number, kind):
+        print('%s is currently %s' % (kind, str(number)))
+        print('Would you like to change it? [Y]es or [N]o')
+        change = input().lower()
+        if change[0] != "y":
+                print('%s number remains %s' % (kind,str(number)))
+        else:
+                print('Change %s from %s to:' % (kind,str(number)))
+                number = int(input())
+                print('New number is  %s' % (str(number)))
+        return number
+
 
 while continueFlag == 1:
-	exit = ["n", "no", "exit", "end"]
 
-	user = nameCheck(user)
-	print('your name is [' + user + ']')
+    exitValues = ["n", "no", "exit", "end"]
 
-	if user == "debug":
-		print('Debug Mode: On')
-		print('\nDebug Mode: Exit is' + str(exit) + '\n\n')
+    user = nameCheck(user)
+    print('your name is [%s]' % (user))
 
-	exit.append(input('What is your safeword?\n').lower())
+    if user == "debug":
+        debug_settings()
 
-	if user == "debug":
-		print('\nDebug Mode: Exit is' + str(exit) + '\n\n')
+    exitValues.append(input('What is your safeword?\n').lower())
 
-	print('Well ' + user + ', I am thinking of a number between 1 and 20')
-	secretNumber = random.randint(1,20)
+    if user == "debug":
+        print('\nDebug Mode: Exit is %s \n\n' % (str(exitValues)))
 
-	if user == "debug":
-		print('>@@ Debug Mode: Secret Number is ' + str(secretNumber) + '  @@<')
+    print('Well %s, I am thinking of a number between %s and %s' % (user, str(low), str(high)))
+    secretNumber = random.randint(low,high)
 
-	for guessCount in range(1,7):
-		print('Take a guess.')
-		guess = int(input())
+    if user == "debug":
+        print('>@@ Debug Mode: Secret Number is %s @@<' % (str(secretNumber)))
 
-		if guess < secretNumber:
-			print('Your guess is too low.')
-		elif guess > secretNumber:
-			print('Your guess is too high.')
-		else:
-			break
+    for guessCount in range(1,7):
+        print('Take a guess.')
+        guess = int(input())   #Need to sanitize this input in case someone puts in words
 
-	if guess == secretNumber:
-		print('Good job, ' + user + '! You guessed my number in ' + str(guessCount) + ' guesses!')
-	else:
-		print('Nope. the number I was thinking of was ' + str(secretNumber))
-	print('Would you like to play again? Enter (n) to exit or your safeword.')
-	playAgain=input().lower()
-	if playAgain in exit:
-		continueFlag = 0
+        if guess < secretNumber:
+            print('Your guess is too low.')
+        elif guess > secretNumber:
+            print('Your guess is too high.')
+        else:
+            break
+
+    if guess == secretNumber:
+        print('Good job, ' + user + '! You guessed my number in ' + str(guessCount) + ' guesses!')
+    else:
+        print('Nope. the number I was thinking of was ' + str(secretNumber))
+    print('Would you like to play again? Enter (n) to exit or your safeword.')
+    playAgain=input().lower()
+    if playAgain in exitValues:
+        continueFlag = 0
