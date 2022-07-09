97454a3a4a729d2da8b8b7fa9e6312e57ecb5172
wolfkin-gungrave <wolfkin@gmail.com>
2022-07-09 15:12:15 -0400

    just adding some stuff about launching from outside idle

---
+++ original/Sec.09/Test.py
@@ -0,0 +1,7 @@
+#! python3
+
+import sys
+
+print('Hello World')
+print('Command-Line Argument Variables')
+print(sys.argv)
---
+++ original/Sec.09/test.bat
@@ -0,0 +1,2 @@
+@py R:\Documents\Projects\ImprovingATBS\original\Sec.09\Test.py %*
+@pause
